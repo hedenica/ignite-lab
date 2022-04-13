6cf76e29f09b96e44a190b6a94a836da16659758
hedenica <hedenica@hotmail.com>
2022-04-13 17:24:05 -0300

    feat: add graphql, codegen, apollo client, tailwind

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,12 +1,22 @@
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { GetServerSideProps } from 'next';
+import { withApollo } from '../../lib/withApollo';
+import { useMeQuery } from '../../graphql/generated/graphql';
+import { getServerPageGetProducts, ssrGetProducts } from '../../graphql/generated/page';
 
-export default function Home() {
+function Home({ data }) {
   const { user } = useUser()
+  const { data: me } = useMeQuery()
 
   return (
-    <div>
+    <div className='text-violet-500'>
       <h1>hello world</h1>
+      <pre>
+        ok: {JSON.stringify(me, null, 2)}
+      </pre>
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
       <pre>
         {JSON.stringify(user, null, 2)}
       </pre>
@@ -14,4 +24,15 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = withPageAuthRequired();
+export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
+  getServerSideProps: async (context) => {
+    // getServerPageGetProducts({}, context)
+    return {
+      props: {}
+    }
+  }
+});
+
+export default withApollo(
+  ssrGetProducts.withPage()(Home)
+);
